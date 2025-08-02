7f5b3b89ae9b08802c40e00c238bd7ca87b367bf
adityapandey23 <adityapandey2395@gmail.com>
2025-08-02 09:26:51 +0530

    added new lsp thingy

---
+++ lua/config/lsp/diagnostics.lua
@@ -0,0 +1,27 @@
+-- lua/config/lsp/diagnostics.lua
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "always",
+  },
+})
+
+vim.o.updatetime = 250
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end,
+})
+
+-- Optional: prettier diagnostic signs
+local signs = { Error = " ", Warn = " ", Hint = "󰌵", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,40 +1,51 @@
+-- I've added the following language servers
+-- 1. lua: for lua
+-- 2. clang: for C and C++
+-- Dear future me, please remember that you add rest of the language servers via Mason, please!
+
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-	"folke/lazydev.nvim",
-	ft = "lua", -- only load on lua files
-	opts = {
-	  library = {
-	    -- See the configuration section for more details
-	    -- Load luvit types when the `vim.uv` word is found
-	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	  },
+    {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+	    {
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+		    library = {
+			-- See the configuration section for more details
+			-- Load luvit types when the `vim.uv` word is found
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+		    },
+		},
+	    },
 	},
-      },
+	config = function()
+	    require("lspconfig").lua_ls.setup {}
+	    -- require("lspconfig").gopls.setup{}
+	    -- require("lspconfig").ts_ls.setup{}
+	    --[[
+	    require("lspconfig").jdtls.setup{
+		cmd = {"/home/aditya/.local/bin/jdtls"}
+	    }
+	    ]]
+	    require("lspconfig").clangd.setup{
+		cmd = {"/usr/bin/clangd"}
+	    }
+
+	    require("config.lsp.diagnostics") -- ✅ load diagnostics config
+	end,
     },
-    config = function()
-      require("lspconfig").lua_ls.setup {}
-      require("lspconfig").gopls.setup{}
-      require("lspconfig").ts_ls.setup{}
-      require("lspconfig").jdtls.setup{
-	cmd = {"/home/aditya/.local/bin/jdtls"}
-      }
-      require("lspconfig").clangd.setup{
-	cmd = {"/usr/bin/clangd-18"}
-      }
-    end,
-  },
-  {
-    'nvim-java/nvim-java',
-    dependencies = {
-      "nvim-java/lua-async-await",
-      "nvim-java/nvim-java-core",
-      "nvim-java/nvim-java-test",
-      "nvim-java/nvim-java-dap",
-      "mfussenegger/nvim-dap",
-      "neovim/nvim-lspconfig",
-    },
-  }
+    -- I really don't remember what the fuck is this
+    -- I'll def need this
+    {
+	'nvim-java/nvim-java',
+	dependencies = {
+	    "nvim-java/lua-async-await",
+	    "nvim-java/nvim-java-core",
+	    "nvim-java/nvim-java-test",
+	    "nvim-java/nvim-java-dap",
+	    "mfussenegger/nvim-dap",
+	    "neovim/nvim-lspconfig",
+	},
+    }
 }
